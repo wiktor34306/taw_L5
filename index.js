39a7092fcb8e47b73e296e7d2df1a074b153a4fd
wiktor34306 <92991883+wiktor34306@users.noreply.github.com>
2023-05-03 15:44:35 +0200

    laboratory7

--- index.js
+++ index.js
@@ -1,21 +1,2 @@
-// const http = require('http');
-
-// const server = http.createServer();
-// server.on('connection', (socket => {
-//    console.log('new connection');
-// }))
-
-// server.listen(3000);
-
-const express = require('express');
-const config = require('./config').config;
-const questions = require('./questions').questions;
-const app = express();
-
-app.get('/api/questions', (request, response) => {
-    response.send(questions);
-});
-
-app.listen(config.port, function () {
-    console.info(`Server is running at port 3000`);
-});
+require( 'babel-core/register' );
+require( './app/app');
