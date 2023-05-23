c9e244c5ff57ed2e0807e865fafbf584d7699dc8
wiktor34306 <92991883+wiktor34306@users.noreply.github.com>
2023-05-23 19:06:51 +0200

    laboratory10

--- server-main/app/REST/post.endpoint.js
+++ server-main/app/REST/post.endpoint.js
@@ -10,6 +10,17 @@ const postEndpoint = (router) => {
         }
     });
 
+    router.get("/api/post/:id", async (request, response, next) => {
+        try {
+            const id = request.params.id;
+
+            let result = await business.getPostManager().get(id);
+            response.status(200).send(result);
+        } catch (error) {
+            console.log(error);
+        }
+    });
+
     router.post('/api/posts', async (request, response, next) => {
         try {
             let result = await business.getPostManager().createNewOrUpdate(request.body);
