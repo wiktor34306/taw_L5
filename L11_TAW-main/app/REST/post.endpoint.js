4e0e1e479d40272a40698ed70eb3d13e6510d60d
wiktor34306 <92991883+wiktor34306@users.noreply.github.com>
2023-05-24 23:05:58 +0200

    laboratory11

--- L11_TAW-main/app/REST/post.endpoint.js
+++ L11_TAW-main/app/REST/post.endpoint.js
@@ -8,5 +8,26 @@ const postEndpoint = (router) => {
             console.log(error);
         }
     });
+
+    router.get("/api/post/:id", async (request, response, next) => {
+        try {
+          const id = request.params.id;
+          const result = await business.getPostManager().get(id);
+          response.staus(200).send(result);
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    
+      router.post("/api/post", async (request, response, next) => {
+        try {
+          const data = request.body;
+          const result = await business.getPostManager().createNewOrUpdate(data);
+          response.status(201).send(result);
+        } catch (error) {
+          console.log(error);
+        }
+      });
 };
 export default postEndpoint
+
